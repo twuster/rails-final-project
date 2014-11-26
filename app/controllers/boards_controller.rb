058aaed5ffd459e4e151a1b11650e178b0fcea45
Tony Wu <tony.wu@berkeley.edu>
2014-11-25 16:41:48 -0800

    Deleted weird vim files and added show view

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -4,7 +4,15 @@ def new
 end
 
 def create
-	render plain: params[:board].inspect
+	@board = Board.new(board_params)
+
+	@board.save
+	redirect_to @board
 end
+
+private
+  def board_params
+    params.require(:board).permit(:title)
+  end
 
 end
