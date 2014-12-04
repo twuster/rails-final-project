23c8c703667a439d341b2973897c9c71d2e992a5
Kevin TianXiang Wu <kevintxwu@gmail.com>
2014-12-04 01:14:34 -0800

    this is so effed up

--- db/migrate/20141125233929_create_pins.rb
+++ db/migrate/20141125233929_create_pins.rb
@@ -2,7 +2,6 @@ class CreatePins < ActiveRecord::Migration
   def change
     create_table :pins do |t|
       t.string :title
-      t.string :user_id
       t.decimal :latitude
       t.decimal :longitude
 
--- db/migrate/20141204064940_change_column_in_pins.rb
+++ db/migrate/20141204064940_change_column_in_pins.rb
@@ -1,5 +1,4 @@
 class ChangeColumnInPins < ActiveRecord::Migration
 	def change
-		rename_column :pins, :user_id, :board_id
 	end
 end
